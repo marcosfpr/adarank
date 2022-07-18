2a31006c21b05b6030ffc2d4a1e301df2720b684
Pontes, Marcos <marcos.pontes@dell.com>
2022-07-18 17:35:35 -0300

    testing logging stuff

--- examples/log.rs
+++ examples/log.rs
@@ -1,8 +1,10 @@
-extern crate ltrs;
+#[macro_use] extern crate prettytable;
+#[macro_use] extern crate ltrs;
 
 use ltrs::utils::logging::*;
 
 use log::{debug, error, info, trace, warn};
+use prettytable::{Table, Row, Cell};
 
 fn main() {
     
@@ -13,5 +15,20 @@ fn main() {
     warn!("{}", "Warning message");
     error!("{}", "Error message");
     trace!("{}", "Trace message");
+
+     // Create the table
+    let mut table = create_table();
+
+     // Add a row per time
+     table.add_row(row!["ABC", "DEFG", "HIJKLMN"]);
+     table.add_row(row!["foobar", "bar", "foo"]);
+     // A more complicated way to add a row:
+     table.add_row(Row::new(vec![
+         Cell::new("foobar2"),
+         Cell::new("bar2"),
+         Cell::new("foo2")]));
+ 
+     // Print the table to stdout
+     info!("\n{}", table.to_string())
 
 }
--- src/utils/logging.rs
+++ src/utils/logging.rs
@@ -1,21 +1,24 @@
+use log::Level;
 /// Copyright (c) 2021 Marcos Pontes
 /// MIT License
 ///
 use std::io::Write;
-use log::{Level};
 
+use colored::{ColoredString, Colorize};
 use env_logger;
-use colored::{Colorize, ColoredString};
-// use prettytable::{Table, Row, Cell};
+use prettytable::{
+    format::{FormatBuilder, TableFormat},
+    Cell, Row, Table,
+};
 
 
 ///
 /// Initializes the logger with the environment variable `RUST_LOG`.
 /// If the variable is not set, the default logger (err) is used.
-/// 
+///
 pub fn init_logger() {
-    env_logger::builder().format(
-        |buf, record| {
+    env_logger::builder()
+        .format(|buf, record| {
             writeln!(
                 buf,
                 "[{}] [{}] [{}] [{}:{}] {}",
@@ -26,13 +29,31 @@ pub fn init_logger() {
                 record.line().unwrap_or(0),
                 record.args()
             )
-        }
-    ).init(); 
+        })
+        .init();
+}
+
+pub fn create_table() -> prettytable::Table {
+    let mut table = Table::new();
+    let format = prettytable::format::FormatBuilder::new()
+        .column_separator('|')
+        .borders('|')
+        .separators(
+            &[
+                prettytable::format::LinePosition::Top,
+                prettytable::format::LinePosition::Bottom,
+            ],
+            prettytable::format::LineSeparator::new('-', '+', '+', '+'),
+        )
+        .padding(1, 1)
+        .build();
+    table.set_format(format);
+    table
 }
 
 ///
 /// Colorize the record level.
-/// 
+///
 fn color_by_level(level: Level) -> ColoredString {
     match level {
         Level::Error => "Error".red(),
