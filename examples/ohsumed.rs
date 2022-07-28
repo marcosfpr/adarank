b264e3287df59a53e70964979d235daf1ee9324f
marcosfpr <mfprezende@gmail.com>
2022-07-28 13:57:03 -0300

    remove env var dependencies

--- examples/ohsumed.rs
+++ examples/ohsumed.rs
@@ -10,24 +10,29 @@ use ltrs::{
 fn main() {
     init_logger();
 
-    // Get ohsumed location from environment variable OHSUMED
-    let corpus_path = std::env::var("OHSUMED").unwrap_or_else(|_| {
-        panic!("Please set the environment variable OHSUMED to the location of the ohsumed dataset")
-    });
+    // Get ohsumed location from the root project path /examples/ohsumed
+    let corpus = std::path::Path::new("benchmarks/OHSUMED").join("Data/All/OHSUMED.txt");
 
-    let ohsumed_dataset: DataSet = ltrs::loader::svmlight::SVMLight::load(
-        format!("{}/Data/All/OHSUMED.txt", corpus_path).as_str(),
-    )
-    .unwrap_or_else(|_| {
-        panic!(
-            "Could not load ohsumed dataset located at {}/Data/All/OHSUMED.txt",
-            corpus_path
+    if corpus.exists() {
+        log::info!("Loading corpus from {}", corpus.display());
+        let ohsumed_dataset: DataSet = ltrs::loader::svmlight::SVMLight::load(
+            corpus.to_str().unwrap()
         )
-    });
+        .unwrap_or_else(|_| {
+            panic!(
+                "Could not load ohsumed dataset located at {}/Data/All/OHSUMED.txt",
+                corpus.display()
+            )
+        });
+    
+        let mut adarank = AdaRank::new(ohsumed_dataset, Box::new(MAP), 50, 3, 0.003, None, None);
+    
+        adarank.fit().unwrap();
+    
+        log::debug!("Finished fitting");
+    } else {
+        log::error!("Corpus not found at {}", corpus.display());
+        std::process::exit(1);
+    }
 
-    let mut adarank = AdaRank::new(ohsumed_dataset, Box::new(MAP), 50, 3, 0.003, None, None);
-
-    adarank.fit().unwrap();
-
-    log::debug!("Finished fitting");
 }
